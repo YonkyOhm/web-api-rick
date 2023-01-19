4407c141e152f76d4319c553072e6ca86d7a7b67
Raul <rulas@gmail.com>
2023-01-19 13:59:41 -0600

    se creo un pipe para leer el objeto del api

---
+++ src/app/pipes/objToArray.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: 'ObjToArray'
+})
+
+export class ObjToArrayPipe implements PipeTransform{
+
+    transform(object: any = []):any {
+        console.log(object)
+        return Object.values(object);
+
+    }
+}
