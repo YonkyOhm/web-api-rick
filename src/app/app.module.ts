bbc8d37904533bfae24242dbc1b0ac7e9c933d48
Raul <rulas@gmail.com>
2023-01-24 16:12:50 -0600

    Eliminacion de componentes que no se usaron

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormularioComponent } from './components/formulario/formulario.component';
+
 
 import { ObjToArrayPipe } from './pipes/objToArray.pipe';
-import { EpisodiosComponent } from './components/episodios/episodios.component';
+
 import { DetallesComponent } from './components/detalles/detalles.component';
 import { FormsModule } from '@angular/forms';
 import { RickModule } from './rick/rick.module';
@@ -15,11 +15,8 @@ import { RickModule } from './rick/rick.module';
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioComponent,
     ObjToArrayPipe,
-    EpisodiosComponent,
     DetallesComponent,
-    
   ],
   imports: [
     BrowserModule,
